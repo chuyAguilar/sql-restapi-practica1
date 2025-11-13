bf663e4cc461c6ef9a54bca7f30a67477dcf7e24
chuyAguilar <jesusaguilar32265@gmail.com>
2025-11-12 19:13:22 -0600

    configs

--- src/config.js
+++ src/config.js
@@ -4,12 +4,12 @@ config();
 
 export const PORT = process.env.PORT || 3000;
 
-export const Db_host = process.env.host || localhost;
+export const Db_host = process.env.host || mysql.railway.internal;
 
 export const Db_user = process.env.user || root;
 
-export const Db_password = process.env.password || 1234;
+export const Db_password = process.env.password || DMQaCEETzBpbpHfQAJPrSXqpNmXDrjnI;
 
-export const Db_database = process.env.database || companydb;
+export const Db_database = process.env.database || railway;
 
 export const Db_port = process.env.portdb || 3306;
